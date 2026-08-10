e677e904caa903536e0594cb3cfa4995a7f87316
Nadzeya Silchankava <sinadzeya@Nadzeyas-MacBook-Pro-3.local>
2026-08-10 17:53:32 +0200

    fix: delete unused <Provider>

--- Week_2_Recipe_Book_Application_(Next.js)/recipe-app/app/layout.js
+++ Week_2_Recipe_Book_Application_(Next.js)/recipe-app/app/layout.js
@@ -1,14 +1,37 @@
 import './globals.css';
 import Providers from './providers';
+import {fetchRecipes as fetchRecipesApi} from '@/api/recipeApi';
+import {Difficulty} from "../store/recipeSlice";
 
-export default function RootLayout({children}) {
+export const metadata = {
+    title: 'Recipe Finder',
+    description: 'Search and browse delicious recipes',
+};
+
+export default async function RootLayout({children}) {
+
+    const initialRecipes = await fetchRecipesApi('', 0);
+
+    const initialReduxState = {
+        recipes: {
+            filter: Difficulty.All,
+            keyword: '',
+            recipes: initialRecipes,
+            loading: false,
+            currentPage: 0,
+            selectedRecipe: null,
+            selectedRecipeLoading: false,
+        },
+    };
+
+
     return (
         <html lang="en">
-        <body>
-        <Providers>
-            {children}
-        </Providers>
-        </body>
+            <body>
+                <Providers initialReduxState={initialReduxState}>
+                    {children}
+                </Providers>
+            </body>
         </html>
     );
 }
