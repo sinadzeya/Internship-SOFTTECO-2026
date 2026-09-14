c1bdba44cebbe5be237c98d7dc248770e0b58bda
Nadzeya Silchankava <sinadzeya@Nadzeyas-MacBook-Pro-3.local>
2026-09-14 15:34:37 +0200

    fix: fix deploy

--- Week_9_ConnectHub/connect-hub/src/main.ts
+++ Week_9_ConnectHub/connect-hub/src/main.ts
@@ -11,7 +11,9 @@ async function bootstrap() {
 
   app.enableCors({
     origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
   app.setGlobalPrefix('api');
